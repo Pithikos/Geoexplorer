fd742f0e8200a13a88a3e304a2e9975c1abf4773
Johan <manossef@hotmail.com>
2013-07-15 21:21:19 +0200

    Small update with etree parsing of Google Response

--- classes/Scanner.py
+++ classes/Scanner.py
@@ -17,37 +17,58 @@ class Scanner:
    # Vars
    bounds    = None    #bounds in latitude and longtitude [x, y, x2, y2]
    
+   
    def __init__(self, bounds):
       self.bounds=bounds
       self.GUI = GUI()
+
 
    def start_scanning(self, tool, args):
       if not self.limiter:
          print("Scanning without limiter will not filter out any results")
       
-      # FOR EVERY SUB-BOX OF THE BOUNDS
-      
+      # FOR EVERY SUB-AREA IN BIG BOX
       if (tool == 'textsearch'):
          print("Textsearch: ", args)
          loc="58.4,15.58"
          rad= 10000
          xml = G_textsearch(args)
          root = etree.fromstring(xml)
-         print(etree.tostring(root).decode("utf-8"))
+         
+     # Get location of each result
+      #while root
+      #print(etree.tostring(root).decode("utf-8"))
+      status = root[0].text
+      token  = None
       
+      if (status == 'OK'):
+         root.remove(root[0])
+      else:
+         return
+         
+      if (root[-1].tag == "next_page_token"):
+         token=root[-1].text
+         root.remove(root[-1])
+
+      #DO SOMETHING WITH NEXT_TOKEN
+      for child in root:
+         print(child.tag)
+
       # SEND EVERY POSITION TO GUI
       self.GUI.add_marker(loc)
+
 
    def stop_scanning(self):
       pass
       
+      
    def set_scheduler(self, scheduler):
       self.scheduler=scheduler
-      return True
+      
       
    def set_limiter(self, limiter):
       self.limiter=limiter
-      return True
+      
       
    def show_config(self):
       print('Limiter:   ', self.limiter)
