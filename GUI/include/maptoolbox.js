da04bca624ef5e810bab4efc48ab3722f35d88d3
Johan <manossef@hotmail.com>
2013-12-01 16:58:17 +0100

    Old garbage remains

--- GUI/include/maptoolbox.js
+++
@@ -1,8 +0,0 @@
-/*-----------------------------------------------------------------------------
- Needs http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false
- -----------------------------------------------------------------------------*/
-"use strict"
-
-function addMarker(lat, lng) {
-   msg("Will add marker at: "+lat+", "+lng)
-}
