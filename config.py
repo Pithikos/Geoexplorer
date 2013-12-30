ce781ba600df41f13f585c729f2b22910d9cc698
Johan <manossef@hotmail.com>
2013-12-30 14:35:20 +0100

    Removed Nominatim & country filtering

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# Configuration values for application
+# This is the configuration for the program
+# Some of these options will be overriden by some services
 config={
 
    # Port for GUI
    'GUI_PORT': 9017,
 
    # Log files
-   'LOG_PATH'             : '/log',
+   'LOG_PATH'             : '/log',       # Directory where to store the log files
    'LOG_SCAN_FILENAME'    : 'scan.log',   # Name of log for the scanning process
    'LOG_STATS_FILENAME'   : 'stats.log',  # Name of log for statistics
    'LOG_RESULT_FILENAME'  : 'result.log', # Name of log for results acquired from the scanning
@@ -15,7 +16,7 @@ config={
    'box':
    {
       'MAX_X_DISTANCE'   : 100000,  # max x distance in m.
-      'MAX_Y_DISTANCE'   : 100000,  # max x distance in m.
+      'MAX_Y_DISTANCE'   : 100000,  # max y distance in m.
    },
 
    # Costs
@@ -24,7 +25,7 @@ config={
       'PER_REQUEST' :  1, # Overriden by some services
    },
 
-   # Limit the results from scanning
+   # Limit the search area
    'limiter':
    {
       #'BOUNDS':  (85, 10, 0, 65), # Lat, Lng, Lat2, Lng2
@@ -35,8 +36,6 @@ config={
       'BOUNDS':  (60, 17, 59, 19), #stockholm 4x4
       #'BOUNDS':  (60, 17, 57, 19), #stockholm+gotland
       #'BOUNDS':  (59.5, 17.38, 58.9, 18.7), # Don't scan outside these bounds
-      'COUNTRY_CODE': 'se',                  # Scan only inside specific country
-      'COUNTRY_CHECK_METHOD': 'NOMINATIM', # Should be either FROMRADARSEARCH or NOMINATIM
    },
 
    # Schedule the timings of the scanning
